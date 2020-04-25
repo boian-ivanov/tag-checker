721f7615ff2d73a67bf2801886c1cec7b0297f36
Boian Ivanov <boian.ivanov@zavamed.com>
2020-04-25 10:15:57 +0100

    Updated fetch function to not brake whenever it returns 500. Updated tags list to be used as array for easier usage across app. Updated functions to be easier to understand. Added methods for removing duplicates, cleaning up based on blacklisted tags & cleaning the whole box.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,20 +5,23 @@
       <b-input v-model="queryStr" v-on:keyup.enter.native="getTags"></b-input>
     </b-field>
 
-    <b-field>
-      <b-button @click="getTags">Submit</b-button>
-    </b-field>
-    <!--    <h2> {{ tags }} </h2>-->
+    <div class="buttons">
+      <b-button @click="getTags" type="is-primary">Submit</b-button>
+    </div>
+
     <b-field label="Result">
-      <b-input type="textarea" v-model="tags"></b-input>
+      <b-input type="textarea" v-model="tagsString"></b-input>
     </b-field>
 
-    <b-field>
-      <b-button @click="copyTags">Copy</b-button>
-    </b-field>
+    <div class="buttons">
+      <b-button @click="copyTags" type="is-success">Copy</b-button>
+      <b-button @click="removeDuplicateTags" type="is-warning">Remove duplicates</b-button>
+      <b-button @click="cleanUpList" type="is-info">Remove unwanted</b-button>
+      <b-button @click="clearTags" type="is-danger">Clean tags</b-button>
+    </div>
 
-    <div><strong>Count : </strong>{{str_arr}}</div>
-    <div><strong>Duplicated : </strong>{{hashtags}}</div>
+    <div><strong>Count : </strong>{{numberOfTags}}</div>
+    <div><strong>Duplicated : </strong>{{duplicateTags}}</div>
   </Layout>
 </template>
 
@@ -31,34 +34,65 @@ export default {
   },
   data: () => ({
     queryStr: '',
-    endpoint: '.netlify/functions/getTags',
-    tags: ''
+    endpoint: '/.netlify/functions/getTags',
+    arrayTags: [],
+    blacklist: [
+      '#bhfyp', '#like', '#likes', '#likeforlikes', '#ig', '#instagram', '#follow',
+      '#followforfollow', '#me', '#girl', '#memes', '#selfie', '#boy', ]
   }),
   methods: {
     async getTags () {
-      const loadingComponent = this.$buefy.loading.open({container:null})
-      let response = await axios.get(this.endpoint + '?tag=' + this.queryStr)
-      loadingComponent.close()
+      const loadingComponent = this.$buefy.loading.open({ container: null })
+      try {
+        let response = await axios.get(this.endpoint + '?tag=' + this.queryStr)
 
-      this.tags = this.tags === '' ? response.data.tags : this.tags + ' ' + response.data.tags
+        this.makeTagsIntoArray(response.data.tags)
+        this.resetQueryString()
+      } catch (e) {
+        this.$buefy.dialog.alert('Something went wrong! It\'s possible that the requested tag doesn\'t exist')
+      }
+      loadingComponent.close()
+    },
+    makeTagsIntoArray (tags) {
+      this.arrayTags = this.arrayTags.concat(tags.split(' '))
+    },
+    resetQueryString () {
       this.queryStr = ''
     },
     copyTags () {
-      this.$copyText(this.tags)
+      this.$copyText(this.tagsString)
+    },
+    removeDuplicateTags () {
+      this.arrayTags = this.removeDuplicateFromArray(this.arrayTags, this.duplicateTags)
+    },
+    cleanUpList () {
+      this.arrayTags = this.removeDuplicateFromArray(this.arrayTags, this.blacklist)
+      this.arrayTags = this.arrayTags.filter(el => el.length > 3)
+    },
+    clearTags () { this.arrayTags = [] },
+    /**
+     * Remove elements of one array from another
+     *
+     * @param array - Array to remove elements from
+     * @param elementsToRemove - Array of elements to remove from first parameter
+     * @return Array without the given elements of second array
+     */
+    removeDuplicateFromArray (array, elementsToRemove) {
+      return array.filter(el => !elementsToRemove.includes(el))
     }
   },
   computed: {
-    arrayTags () {
-      return this.tags.split(' ')
-    },
-    hashtags () {
+    duplicateTags () {
       return this.arrayTags.reduce(function (acc, el, i, arr) {
         if (arr.indexOf(el) !== i && acc.indexOf(el) < 0) acc.push(el)
         return acc
       }, [])
     },
-    str_arr () {
-      return this.tags.length > 0 ? this.arrayTags.length : ''
+    tagsString () {
+      return this.arrayTags.join(' ')
+    },
+    numberOfTags () {
+      return this.arrayTags.length
     }
   }
 }
